64e61e7cafc2a6f2dc3e8face18d95b4806afdb4
Moisés H. Quintanilla <qm09005@ues.edu.sv>
2024-03-24 16:14:55 -0600

    endpoint de logs

--- controllers/mediciones.js
+++ controllers/mediciones.js
@@ -7,12 +7,30 @@ const getUserQuery = async (req, res) => {
     });
 }
 const sensoresquery = async (req, res) => {
-const results  = await queryRun("select * from sensores order by id desc limit 1");
-res.json({
+    const {
+        query:{
+            page=1,
+            elements=1000,
+            orderBy='id'
+        }
+    } = req
+    const limiteSuperior = (page)*elements;
+    const limiteInferior = (page-1) * elements + 1
+const results  = await queryRun(`select * from TBE order by ${orderBy} desc limit ${limiteInferior},${limiteSuperior}`);
+res.json(
     results,
-});
+);
 }
+
+const logsErrores = async (req, res)=>{
+    const results = await queryRun(`select * from LOG order by id desc`)
+    res.json(
+        results
+    )
+}
+
 module.exports = {
     sensoresquery,
-    getUserQuery
+    getUserQuery,
+    logsErrores
 };
--- routes/mediciones.js
+++ routes/mediciones.js
@@ -1,8 +1,13 @@
-const { Router } = require('express');
-const { validarjwt } = require('../middlewares');
-const { getUserQuery, sensoresquery } = require('../controllers/mediciones');
+const { Router } = require("express");
+const { validarjwt } = require("../middlewares");
+const {
+  getUserQuery,
+  sensoresquery,
+  logsErrores,
+} = require("../controllers/mediciones");
 const router = Router();
 
-router.get('/queryUser', [validarjwt], getUserQuery);
-router.get("/sensores", [],sensoresquery)
+router.get("/queryUser", [validarjwt], getUserQuery);
+router.get("/sensores", [], sensoresquery);
+router.get("/logs", [], logsErrores);
 module.exports = router;
