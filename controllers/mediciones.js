e4c12fbdb11d7614ce10e70331af1c9ace0d8c75
Moisés H. Quintanilla <qm09005@ues.edu.sv>
2024-04-07 11:57:43 -0600

    Cambios de escritura en tabla de control

--- controllers/mediciones.js
+++ controllers/mediciones.js
@@ -1,4 +1,4 @@
-
+var moment = require('moment');
 const { queryRun } = require('../db/query');
 const getUserQuery = async (req, res) => {
     const results  = await queryRun("select * from Users");
@@ -29,8 +29,35 @@ const logsErrores = async (req, res)=>{
     )
 }
 
+const ultimoControl = async (req, res)=>{
+    const results = await queryRun('select * from CONTROL order by id desc limit 1')
+    res.json(results[0] ?? {})
+}
+
+const escribirControl = async (req, res)=>{
+    const {
+        body:{
+            body:{
+                bomba,
+                iluminacion,
+                extractor
+            }
+        }
+    } = req
+    const fecha = moment().format('YYYY-MM-DD HH:mm:ss')
+    console.log('req.body :>> ', req.body.body);
+    try {
+        await queryRun(`INSERT INTO TBE.CONTROL (FECHA, BOMBAAGUA, ILUMINACION, EXTRACTOR) VALUES('${fecha}', ${bomba}, ${iluminacion}, ${extractor}) `)
+    } catch (error) {
+        return res.status(400).json('No se pudo guardar la configuración')
+    }
+    return res.status(201).json('Configuración guardada con éxito')
+}
+
 module.exports = {
     sensoresquery,
     getUserQuery,
-    logsErrores
+    logsErrores,
+    ultimoControl,
+    escribirControl
 };
--- routes/mediciones.js
+++ routes/mediciones.js
@@ -4,10 +4,14 @@ const {
   getUserQuery,
   sensoresquery,
   logsErrores,
+  ultimoControl,
+  escribirControl
 } = require("../controllers/mediciones");
 const router = Router();
 
 router.get("/queryUser", [validarjwt], getUserQuery);
 router.get("/sensores", [], sensoresquery);
 router.get("/logs", [], logsErrores);
+router.get('/control/automatico', [],ultimoControl)
+router.post('/control/manual', [],escribirControl)
 module.exports = router;
